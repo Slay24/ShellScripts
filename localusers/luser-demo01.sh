8aa1ef28965e04323efbd5bbd126862363a46ed8
Slay24 <d.kassaye@outlook.com>
2024-12-20 20:06:30 -0600

    Added a few more info about variables to the luser-demo01.sh

--- localusers/luser-demo01.sh
+++ localusers/luser-demo01.sh
@@ -15,4 +15,15 @@ echo "This is a shell $WORD"
 #Display the contents of the variable using an alternative syntax
 echo "This is a shell ${WORD}"
 
-#Append text to the variable
+# Append text to the variable
+echo "${WORD}ing is fun!"
+
+#Create a new variable
+ENDING='ed'
+
+#Combine the two variables
+echo "This is ${WORD}${ENDING}."
+
+#Change the value stored in the ENDING variable. (Reassignment)
+
+ENDING='ing'
